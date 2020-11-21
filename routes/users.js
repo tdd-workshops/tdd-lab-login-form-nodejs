d700d956114844f6ac57b56ca51ba1ef36a8079b
Paul "Joey" Clark <joeytwiddle@gmail.com>
2020-11-21 15:03:53 +0000

    When password is too short, render welcome message with an error

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,26 @@ const router = express.Router()
 const USERNAME = 'Demo User'
 const USEREMAIL = 'demo@example.com'
 const USERPASSWORD = 'demo1234'
+const USERPASSWORD2 = 'betterPassword3!Q5'
 
 router.post('/', function (req, res, next) {
   // console.log('User credentials:', req.body.email)
 
-  if (req.body.email === USEREMAIL && req.body.password === USERPASSWORD) {
+  // For this exercise, we will allow either password to log in
+  if (req.body.email === USEREMAIL && (req.body.password === USERPASSWORD || req.body.password === USERPASSWORD2)) {
     req.session.username = USERNAME
     req.session.lastLogin = Date.now()
-    res.redirect('/users/welcome')
+
+    if (req.body.password.length < 10) {
+      // If the user's password is too short, render the welcome page, but show a warning
+      res.render('users/welcome', {
+        title: 'Security Alert',
+        warningMsg: 'Your password is too short, please change it to protect your account'
+      })
+    } else {
+      // Otherwise redirect to the welcome page as before
+      res.redirect('/users/welcome')
+    }
   } else {
     res.redirect('/?msg=invalid_credentials')
   }
--- test/users.js
+++ test/users.js
@@ -2,10 +2,20 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('POST /users', function () {
-  it('log in with username and password', function (done) {
+  it('warns user that password is too short', function (done) {
+    // We expect login with a short password to display a warning
     request.agent(app)
       .post('/users')
       .send({ email: 'demo@example.com', password: 'demo1234' })
+      .redirects(5)
+      .expect(200, /Your password is too short/, done)
+  })
+
+  it('log in with username and password', function (done) {
+    // We expect login with a good password to redirect to the welcome page
+    request.agent(app)
+      .post('/users')
+      .send({ email: 'demo@example.com', password: 'betterPassword3!Q5' })
       .redirects(5)
       .expect(200, /Welcome back/, done)
   })
