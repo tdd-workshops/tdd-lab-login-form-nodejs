94b717cf3f7b2c5ae4538943f16a4f7eeca65f86
Paul "Joey" Clark <joeytwiddle@gmail.com>
2020-11-21 13:31:29 +0000

    Use agent to preserve cookies between requests

--- test/users.js
+++ test/users.js
@@ -2,10 +2,18 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('POST /users', function () {
-  it('login with username and password', function (done) {
-    request(app)
+  const agent = request.agent(app)
+
+  it('log in with username and password', function (done) {
+    agent
       .post('/users')
       .send('email=demo@example.com&password=demo1234')
-      .expect(302, /\/welcome/, done)
+      .expect(302, /\/users\/welcome/, done)
+  })
+
+  it('display welcome page after login', function (done) {
+    agent
+      .get('/users/welcome')
+      .expect(200, /Welcome back/, done)
   })
 })
